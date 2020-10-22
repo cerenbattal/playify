b5ec2d4a585411ba9fec9d1fbfb748b887581488
cerenbattal <cerenbattal97@gmail.com>
2020-10-22 21:39:29 +0300

    data layer is added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import SpotifyWebApi from "spotify-web-api-js"
 import "./App.css";
 import Login from "./Login";
+import Player from "./Player"
 import { getTokenFromUrl } from "./spotify";
 
+const spotify = new SpotifyWebApi();
+
 function App() {
+  const [token, setToken] = useState()
+
   // run code based on a given condition
   useEffect(() => {
     const hash = getTokenFromUrl();
     window.location.hash = "";
-    const token = hash.access_token;
-    console.log("The token is: ", token)
+    const _token = hash.access_token;
+
+    if(_token) {
+      console.log("The token is: ", _token)
+      setToken(_token)
+      spotify.setAccessToken(_token)
+      spotify.getMe().then(user => {
+        console.log(user)
+      })
+    }
+    
   }, [])
   return (
-    <div className="App">
-      <Login />
+    <div className="app">
+      {
+        token ? <Player /> : <Login />
+      }
     </div>
   );
 }
