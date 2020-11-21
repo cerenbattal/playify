43ba5cc548e8b4ba9a75248cecf9ee89d7553322
cerenbattal <cerenbattal97@gmail.com>
2020-11-21 22:26:18 +0300

    footer and sidebar is created

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,10 +1,25 @@
 import React from 'react'
 import "./Sidebar.css"
+import SidebarOption from "./SidebarOption"
+import HomeIcon from "@material-ui/icons/Home"
+import SearchIcon from "@material-ui/icons/Search"
+import LibraryMusicIcon from "@material-ui/icons/LibraryMusic"
+import { useDataLayerValue } from './DataLayer'
 
 const Sidebar = () => {
+    const [{ playlists }, dispatch] = useDataLayerValue();
     return (
         <div className="sidebar">
-            <h1>I am the sidebar</h1>
+            <img className="sidebar__logo" src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_RGB_White.png" alt=""/>
+            <SidebarOption title="Home" Icon={HomeIcon} />
+            <SidebarOption title="Search" Icon={SearchIcon} />
+            <SidebarOption title="Your Library" Icon={LibraryMusicIcon} />
+            <br />
+            <strong className="sidebar__title">PLAYLISTS</strong>
+            <hr />
+            {playlists?.items?.map(playlist => (
+                <SidebarOption title={playlist.name}/>
+            ))}
         </div>
     )
 }
